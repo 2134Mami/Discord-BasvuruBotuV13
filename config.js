d8f4fabe7fd803311819939f8061172ea8cef7c6
2134Mami <168979015+2134Mami@users.noreply.github.com>
2024-05-28 12:51:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,30 +1,30 @@
 const config = {
-  TOKEN: "", //Glitchde Kullanacak iseniz .env Dosyasına Tokeni Yazın!
+  TOKEN: "MTIxOTcyNzA0NjgzNDUyNDE2MA.GhZG0u.uHf8WReJXSDmjjmWBCsbEkBYzFztUvAdDoM4K8", //Glitchde Kullanacak iseniz .env Dosyasına Tokeni Yazın!
 
   prefix: "!",
-  guild: "", //Sunucu ID Yazın
-  channel: "", //Başvuru Yap Mesajının Atılacağı Kanal ID Yazın
+  guild: "1158380506447089714", //Sunucu ID Yazın
+  channel: "1233070985008123925", //Başvuru Yap Mesajının Atılacağı Kanal ID Yazın
 
   log: {
-    staff: "", //Başvuru Onaylayıp-Reddedecek Yetkili Rol ID Yazın!
-    channel: "" //Başvuruların Loglanacağı Kanal ID Yazın
+    staff: "1233074923837067284", //Başvuru Onaylayıp-Reddedecek Yetkili Rol ID Yazın!
+    channel: "1244940194541473844" //Başvuruların Loglanacağı Kanal ID Yazın
   },
 
   sonuc: {
-    staffRole: [""], //Sunucunuzdaki Başlangıç Yetkili Rolü veya Rolleri, Birden Fazla Rol Eklemek İçin ["rol id 1", "rol id2"] Başvuru Onaylanınca Buraya Yazılan Rol veya Roller Kullanıcıya Verilecek
-    sRole: "", //Verilecek En Düşük Yetkili Rol ID Yazın
-    successMessage: "Hey Tebrikler Yetkili Oldun!", //Başvuru Onaylanınca Kullanıcıya Atılacak Mesajı Yazın
-    deleteMessage: "Üzgünüm Başvurun Reddedildi!", //Başvuru Reddedilince Kullanıcıya Atılacak Mesajı Yazın
+    staffRole: ["1233075697980018759"], //Sunucunuzdaki Başlangıç Yetkili Rolü veya Rolleri, Birden Fazla Rol Eklemek İçin ["rol id 1", "rol id2"] Başvuru Onaylanınca Buraya Yazılan Rol veya Roller Kullanıcıya Verilecek
+    sRole: "1233075697980018759", //Verilecek En Düşük Yetkili Rol ID Yazın
+    successMessage: "**UNITED KINGDOM** Sunucusunda Yapmış Oldugunuz Yetkili Başvurunuz Onaylanmıştır Ve Rolünüz Verilmiştir Hoşgeldiniz! <a:tik_3:1238551234785443860>", //Başvuru Onaylanınca Kullanıcıya Atılacak Mesajı Yazın
+    deleteMessage: "**UNITED KINGDOM** Sunucusunda Yapmış Oldugunuz Yetkili Başvurunuz Reddedildi!  <a:carpi:1239984008838778971>", //Başvuru Reddedilince Kullanıcıya Atılacak Mesajı Yazın
     timeout: 20000 //Başvurusu Reddedilen Birisi Nekadar Süre Sonra Başvuru Yapa Bilsin? 1000=1 Saniye (Milisaniye Türünden Yazın)
   },
 
   embed: {
     title: "Başvuru Sistemi", //Başvuru Mesajı Başlık Kısmında Yazmasını İstediğiniz Şey
     color: "BLUE", //Embed Rengi, Örnek: BLUE,RED,YELLOW,GREEN Olarak veya Direkt #ff00ff Gibi Renk Kodu Yazın
-    description: "Yetkili Başvurusu Yapa Bilmek İçin Lütfen Aşşağıdaki Butona Basınız!" //Başvuru Mesajı Açıklama Kısmında Yazmasını İstediğiniz Şey
+    description: "- Yetkili başvuru yapmak için aşağıdaki butona tıklaman yeterli.\n\n**Unutma:** Gereksiz başvuru yapmak ceza almana sebebiyet\nverebilir." //Başvuru Mesajı Açıklama Kısmında Yazmasını İstediğiniz Şey
   },
 
-  sorular: ["soru 1", "soru 2", "soru 3"] //Sorulacak Sorular
+  sorular: ["**İsimin Ve Yaşın**", "**Tecrübelerin**", "**Aktiflik Süren**", "**Neler Yapa Bilirsin**", "**Sadece Burda Aktif Ola Bilir Misin**"] //Sorulacak Sorular
 };
 
 module.exports = config;
