60f8e5160b7340373e79c42fc320d37a69457b25
2134Mami <168979015+2134Mami@users.noreply.github.com>
2024-05-28 20:44:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const config = {
-  TOKEN: "MTIxOTcyNzA0NjgzNDUyNDE2MA.GhZG0u.uHf8WReJXSDmjjmWBCsbEkBYzFztUvAdDoM4K8", //Glitchde Kullanacak iseniz .env Dosyasına Tokeni Yazın!
+  TOKEN: "MTIxODM0MDE0MDU3Nzk4MDQzNg.G7-62a.bzI9Clkm__S4ghA32-9FKEEd073pOmyJRwpv68", //Glitchde Kullanacak iseniz .env Dosyasına Tokeni Yazın!
 
   prefix: "!",
-  guild: "1158380506447089714", //Sunucu ID Yazın
-  channel: "1233070985008123925", //Başvuru Yap Mesajının Atılacağı Kanal ID Yazın
+  guild: "1218540046571933736", //Sunucu ID Yazın 
+  channel: "1232654393166528582", //Başvuru Yap Mesajının Atılacağı Kanal ID Yazın
 
   log: {
-    staff: "1233074923837067284", //Başvuru Onaylayıp-Reddedecek Yetkili Rol ID Yazın!
-    channel: "1244940194541473844" //Başvuruların Loglanacağı Kanal ID Yazın
+    staff: "1231616051494785087", //Başvuru Onaylayıp-Reddedecek Yetkili Rol ID Yazın!
+    channel: "1231616222102032434" //Başvuruların Loglanacağı Kanal ID Yazın
   },
 
   sonuc: {
-    staffRole: ["1233075697980018759"], //Sunucunuzdaki Başlangıç Yetkili Rolü veya Rolleri, Birden Fazla Rol Eklemek İçin ["rol id 1", "rol id2"] Başvuru Onaylanınca Buraya Yazılan Rol veya Roller Kullanıcıya Verilecek
-    sRole: "1233075697980018759", //Verilecek En Düşük Yetkili Rol ID Yazın
-    successMessage: "**UNITED KINGDOM** Sunucusunda Yapmış Oldugunuz Yetkili Başvurunuz Onaylanmıştır Ve Rolünüz Verilmiştir Hoşgeldiniz! <a:tik_3:1238551234785443860>", //Başvuru Onaylanınca Kullanıcıya Atılacak Mesajı Yazın
-    deleteMessage: "**UNITED KINGDOM** Sunucusunda Yapmış Oldugunuz Yetkili Başvurunuz Reddedildi!  <a:carpi:1239984008838778971>", //Başvuru Reddedilince Kullanıcıya Atılacak Mesajı Yazın
+    staffRole: ["1231616058951995493"], //Sunucunuzdaki Başlangıç Yetkili Rolü veya Rolleri, Birden Fazla Rol Eklemek İçin ["rol id 1", "rol id2"] Başvuru Onaylanınca Buraya Yazılan Rol veya Roller Kullanıcıya Verilecek
+    sRole: "1231616058951995493", //Verilecek En Düşük Yetkili Rol ID Yazın
+    successMessage: "**COLD ORGANİZASYON** Sunucusunda Yapmış Oldugunuz Yetkili Başvurunuz Onaylanmıştır Ve Rolünüz Verilmiştir! <a:tik_3:1238551234785443860>", //Başvuru Onaylanınca Kullanıcıya Atılacak Mesajı Yazın
+    deleteMessage: "**COLD ORGANİZASYON** Sunucusunda Yapmış Oldugunuz Yetkili Başvurunuz Reddedildi!  <a:carpi:1239984008838778971>", //Başvuru Reddedilince Kullanıcıya Atılacak Mesajı Yazın
     timeout: 20000 //Başvurusu Reddedilen Birisi Nekadar Süre Sonra Başvuru Yapa Bilsin? 1000=1 Saniye (Milisaniye Türünden Yazın)
   },
 
